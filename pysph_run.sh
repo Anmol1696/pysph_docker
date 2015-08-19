2c2fe84d67c38acc32b76f8149d6fc55452af399
Anmol <anmol1696@gmail.com>
2015-08-19 19:19:21 +0530

    Update pysph_run

--- pysph_run.sh
+++ pysph_run.sh
@@ -1,3 +1,3 @@
 xhost +
 XSOCK=/tmp/.X11-unix
-docker run -i -t -v $XSOCK:$XSOCK anmol1696/pysph-docker /bin/bash || docker attach pysph-docker /bin/bash
+docker run --name pysph-docker -i -t -v $XSOCK:$XSOCK anmol1696/pysph-docker /bin/bash || docker attach pysph-docker /bin/bash
