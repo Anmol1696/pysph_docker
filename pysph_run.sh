b2de89bb044fab4b73a3da433d9fff372e53cf30
adityapb <adityapb1546@gmail.com>
2015-08-24 15:57:16 +0530

    Update running script

--- pysph_run.sh
+++ pysph_run.sh
@@ -1,5 +1,5 @@
 xhost +
 XSOCK=/tmp/.X11-unix
-docker run --name pysph-docker -i -t -v $XSOCK:$XSOCK anmol1696/pysph-docker /bin/bash || (docker start pysph-docker /bin/bash && docker attach pysph-docker /bin/bash)
+docker run --name pysph-docker -i -t -v $XSOCK:$XSOCK anmol1696/pysph-docker /bin/bash || (docker start pysph-docker /bin/bash && docker attach pysph-docker)
 # Inorder to view the volume files run "cd MOUNT_VOL"
 MOUNT_VOL=`docker inspect -f '{{(index .Mounts 1).Source}}' pysph-docker`
