b8c7e0b2240c573c44957b3fec179af4462a28de
adityapb <adityapb1546@gmail.com>
2015-08-25 12:19:18 +0530

    Check names of existing containers before running. Fixes #15

--- pysph_run.sh
+++ pysph_run.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 xhost +
 XSOCK=/tmp/.X11-unix
-docker run --name pysph-docker -i -t -v $XSOCK:$XSOCK anmol1696/pysph-docker /bin/bash || (docker start pysph-docker /bin/bash && docker attach pysph-docker)
-# Inorder to view the volume files run "cd MOUNT_VOL"
+CONT=`docker ps -a --filter "name=pysph-docker" --format "{{.ID}}"`
+if [ $CONT != "" ]
+then
+  docker start pysph-docker /bin/bash
+  docker attach pysph-docker
+else
+  docker run --name pysph-docker -i -t -v $XSOCK:$XSOCK anmol1696/pysph-docker /bin/bash
 MOUNT_VOL=`docker inspect -f '{{(index .Mounts 1).Source}}' pysph-docker`
-echo "The mounted volume can be found at $MOUNT_VOL"
